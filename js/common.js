77ab908e8cd5cbbd43f90f3403ca50d8391e4d74
igarbuz <inna.garbuz@accounts.ait.com>
2017-10-19 19:38:04 +0300

    autoservice

---
+++ js/common.js
@@ -0,0 +1,285 @@
+//текст меняется
+var object = {
+    link1: {
+        image: 'img/block3.png',
+        image1: 'img/blok4.jpg',
+        image2: 'img/block1.png',
+        text: 'Header6',
+        subtexttop: 'Loremjihugyft ipsum dolor sit amet, ',
+        subtext: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem itaque explicabo accusamus. Harum quia accusamus soluta dolor fugiat enim necessitatibus esse! Ipsum, saepe eos, obcaecati explicabo incidunt error illo!'
+    },
+    link2: {
+        image: 'img/block3.png',
+        image1: 'img/blok4.jpg',
+        image2: 'img/block1.png',
+        text: 'Header2',
+        subtexttop: 'Loremjihugyft ipsum dolor sit amet, ',
+        subtext: 'Loremjihugyft ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem itaque explicabo accusamus. Harum quia accusamus soluta dolor fugiat enim necessitatibus esse! Ipsum, saepe eos, obcaecati explicabo incidunt error illo!'
+
+    },
+    link3: {
+        image: 'img/block3.png',
+        image1: 'img/blok4.jpg',
+        image2: 'img/block1.png',
+        text: 'Header2',
+        subtexttop: 'Loremjihugyft ipsum dolor sit amet, ',
+        subtext: 'Loremjihugyft ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem itaque explicabo accusamus. Harum quia accusamus soluta dolor fugiat enim necessitatibus esse! Ipsum, saepe eos, obcaecati explicabo incidunt error illo!'
+    },
+    link4: {
+        image: 'img/block3.png',
+        image1: 'img/blok4.jpg',
+        image2: 'img/blok1.png',
+        text: 'Header4',
+        subtexttop: '4Loremjihugyft ipsum dolor sit amet, ',
+        subtext: '4Loremjihugyft ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem itaque explicabo accusamus. Harum quia accusamus soluta dolor fugiat enim necessitatibus esse! Ipsum, saepe eos, obcaecati explicabo incidunt error illo!'
+    },
+    link5: {
+        image: 'img/block3.png',
+        image1: 'img/blok4.jpg',
+        image2: 'img/block1.png',
+        text: 'Header5',
+        subtexttop: '5Loremjihugyft ipsum dolor sit amet, ',
+        subtext: '5Loremjihugyft ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem itaque explicabo accusamus. Harum quia accusamus soluta dolor fugiat enim necessitatibus esse! Ipsum, saepe eos, obcaecati explicabo incidunt error illo!'
+    },
+     link6: {
+        image: 'img/block3.png',
+        image1: 'img/blok4.jpg',
+        image2: 'img/block1.png',
+        text: 'Header5',
+        subtexttop: '5Loremjihugyft ipsum dolor sit amet, ',
+        subtext: '5Loremjihugyft ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem itaque explicabo accusamus. Harum quia accusamus soluta dolor fugiat enim necessitatibus esse! Ipsum, saepe eos, obcaecati explicabo incidunt error illo!'
+    },
+    //blockSensors
+     link7: {
+        image: 'img/block3.png',
+        text: 'Header7',
+        subtexttop: '5Loremjihugyft ipsum dolor sit amet, ',
+        subtext: '5Loremjihugyft ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem itaque explicabo accusamus. Harum quia accusamus soluta dolor fugiat enim necessitatibus esse! Ipsum, saepe eos, obcaecati explicabo incidunt error illo!'
+    },
+     link8: {
+        image: 'img/block1.png',
+        text: 'Header8',
+        subtexttop: '5Loremjihugyft ipsum dolor sit amet, ',
+        subtext: '5Loremjihugyft ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem itaque explicabo accusamus. Harum quia accusamus soluta dolor fugiat enim necessitatibus esse! Ipsum, saepe eos, obcaecati explicabo incidunt error illo!'
+    },
+     link9: {
+        image: 'img/block3.png',
+        text: 'Header9',
+        subtexttop: '5Loremjihugyft ipsum dolor sit amet, ',
+        subtext: '5Loremjihugyft ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem itaque explicabo accusamus. Harum quia accusamus soluta dolor fugiat enim necessitatibus esse! Ipsum, saepe eos, obcaecati explicabo incidunt error illo!'
+    },
+// blockAudio
+    link10: {
+        image: 'img/block3.png',
+
+        text: 'Header10',
+        subtexttop: '5Loremjihugyft ipsum dolor sit amet, ',
+        subtext: '5Loremjihugyft ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem itaque explicabo accusamus. Harum quia accusamus soluta dolor fugiat enim necessitatibus esse! Ipsum, saepe eos, obcaecati explicabo incidunt error illo!'
+    },
+    link11: {
+        image: 'img/block1.png',
+        text: 'Header11',
+        subtexttop: '5Loremjihugyft ipsum dolor sit amet, ',
+        subtext: '5Loremjihugyft ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem itaque explicabo accusamus. Harum quia accusamus soluta dolor fugiat enim necessitatibus esse! Ipsum, saepe eos, obcaecati explicabo incidunt error illo!'
+    },
+    link12: {
+        image: 'img/block3.png',
+        text: 'Header12',
+        subtexttop: '5Loremjihugyft ipsum dolor sit amet, ',
+        subtext: '5Loremjihugyft ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem itaque explicabo accusamus. Harum quia accusamus soluta dolor fugiat enim necessitatibus esse! Ipsum, saepe eos, obcaecati explicabo incidunt error illo!'
+    },
+    //blockSignal
+     link13: {
+        image: 'img/block3.png',
+        image: 'img/block1.png',
+        image: 'img/block3.png',
+        text: 'Header13',
+        subtexttop: '5Loremjihugyft ipsum dolor sit amet, ',
+        subtext: '5Loremjihugyft ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem itaque explicabo accusamus. Harum quia accusamus soluta dolor fugiat enim necessitatibus esse! Ipsum, saepe eos, obcaecati explicabo incidunt error illo!'
+    },
+     link14: {
+        image: 'img/block1.png',
+        image: 'img/block1.png',
+        image: 'img/block3.png',
+        text: 'Header14',
+        subtexttop: '5Loremjihugyft ipsum dolor sit amet, ',
+        subtext: '5Loremjihugyft ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem itaque explicabo accusamus. Harum quia accusamus soluta dolor fugiat enim necessitatibus esse! Ipsum, saepe eos, obcaecati explicabo incidunt error illo!'
+    },
+     link15: {
+        image: 'img/block3.png',
+        image: 'img/block1.png',
+        image: 'img/block3.png',
+        text: 'Header15',
+        subtexttop: '5Loremjihugyft ipsum dolor sit amet, ',
+        subtext: '5Loremjihugyft ipsum dolor sit amet, consectetur adipisicing elit. Optio quidem itaque explicabo accusamus. Harum quia accusamus soluta dolor fugiat enim necessitatibus esse! Ipsum, saepe eos, obcaecati explicabo incidunt error illo!'
+    }
+}
+
+var blockLight = function(link) {
+document.getElementById('img_1').src = object[link].image;
+document.getElementById('img_1.1').src = object[link].image1;
+document.getElementById('img_1.2').src = object[link].image2;
+document.getElementById('head_1').innerHTML = object[link].text;
+document.getElementById('top_1').innerHTML = object[link].subtexttop;
+document.getElementById('text_1').innerHTML = object[link].subtext;
+}
+$(document).ready(function() {
+$('.cbg_big_').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
+});
+var blockSensors = function(link) {
+document.getElementById('img_2').src = object[link].image;
+document.getElementById('head_2').innerHTML = object[link].text;
+document.getElementById('top_2').innerHTML = object[link].subtexttop;
+document.getElementById('text_2').innerHTML = object[link].subtext;
+
+}
+var blockAudio = function(link) {
+document.getElementById('img_3').src = object[link].image;
+document.getElementById('img_3').src = object[link].image;
+document.getElementById('img_3').src = object[link].image;
+document.getElementById('head_3').innerHTML = object[link].text;
+document.getElementById('top_3').innerHTML = object[link].subtexttop;
+document.getElementById('text_3').innerHTML = object[link].subtext;
+
+}
+$(document).ready(function() {
+$('.b4_big_img').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
+});
+var blockSignal = function(link) {
+document.getElementById('img_4').src = object[link].image;
+document.getElementById('head_4').innerHTML = object[link].text;
+document.getElementById('top_4').innerHTML = object[link].subtexttop;
+document.getElementById('text_4').innerHTML = object[link].subtext;
+
+}
+
+// function showID(id) {
+// document.getElementById(id).style.display='block';
+// }
+// function hideID(id) {
+// document.getElementById(id).style.display='none';
+// }
+// function l_image (a) {
+//     document.example_img.src=a
+// }
+// function l_image1 (a) {
+//     document.example_img1.src=a
+// }
+// function l_image2 (a) {
+//     document.example_img2.src=a
+// }
+// function  change_image(event){
+//     $("#id1, #id2, #id3").attr('src',$(event.target).attr('href'));
+// }
+
+// function toggle(el) {
+//   el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+// }
+$(window).load(function() {
+  $('#before-load').find('i').fadeOut().end().delay(400).fadeOut('slow');
+});
+
+ $(document).ready(function() {
+    function heightDetect() {
+    $(".main_head").css("height", $(window).height());
+  };
+  heightDetect();
+  $(window).resize(function () {
+    heightDetect();
+  });
+
+var cont = $(".neon-lit-background");
+var neon = $(".neon-border");
+setInterval(function() {
+  var i = Math.floor((Math.random() * 10) + 1) * 200;
+  setTimeout(function() {
+    setTimeout(function() {
+        $(cont).addClass("neon-lit-background--poweroff");
+        $(neon).addClass("neon-border--poweroff");
+      }, i),
+      $(cont).removeClass("neon-lit-background--poweroff");
+    $(neon).removeClass("neon-border--poweroff");
+  }, 1000);
+}, 2000);
+
+$(".burger-menu").click(function () {
+ $(this).toggleClass("menu-on");
+ $("#menu").slideToggle(500);
+})
+
+
+
+
+$(".banner-button").click(function() {
+  $(this).toggleClass("active");
+});
+
+
+  $(".banner-button").first().addClass("active");
+
+  var amountOfSlides = $('.banner-button').length;
+
+  for (var i = 0; i < amountOfSlides; i++) {
+    var current = $(".active");
+    var next = current.next();
+    setInterval(function() {
+    current.removeClass("active");
+      next.addClass("active");
+    }, 800);
+  }
+
+$("#menu a").mPageScroll2id();
+$(".gallary_block a").mPageScroll2id();
+$('.bl_h3 a').mPageScroll2id();
+//      $('#menu ').css('display', 'none');
+$("#gallery a").mPageScroll2id();
+// })
+
+$("#phone-number a").mPageScroll2id();
+$("#menu a").click(function () {
+   $('#menu ').css('display', 'none');
+  $('.burger-menu').toggleClass("menu-on");
+})
+/*
+});
+ $(document).ready(function() {*/
+    $('a[name=modal]').click(function(e) {
+      e.preventDefault();
+      var id = $(this).attr('href');
+      var backHeight = $(document).height();
+      var backWidth = $(window).width();
+      $('#back').css({'width':backWidth,'height':backHeight});
+      $('#back').fadeIn(300);
+      $('#back').fadeTo(300,0.8);
+      var winH = $(window).height();
+      var winW = $(window).width();
+      $(id).css('top',  winH/2-$(id).height()/2);
+      $(id).css('left', winW/2-$(id).width()/2);
+      $(id).fadeIn(300);
+    });
+    $('.window .close').click(function (e) {
+      e.preventDefault();
+      $('#back, .window').hide();
+    });
+    $('#back').click(function () {
+      $(this).hide();
+      $('.window').hide();
+    });
+
+  });
+
+
+// <button onclick="stateChanger('link1')">link 1</button>
+// <button onclick="stateChanger('link2')">link 2</button>
